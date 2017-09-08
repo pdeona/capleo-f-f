22381833a7a0951dddd00287f510bd98145d8d77
Pedro De Ona <Pedro De Ona>
2017-09-08 09:47:28 -0400

    add reviews scaffold TODO: finish building out review controller, views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
+  has_many :reviews
 
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908134046) do
+ActiveRecord::Schema.define(version: 20170908134549) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170908134046) do
     t.integer "price_in_cents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
 end
